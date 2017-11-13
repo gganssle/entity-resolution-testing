e27027d08c32e69ae500c3844ba95a9a211348ca
gram <gr@m-gan.sl>
2017-11-13 16:56:30 -0600

    adding new data JSON

---
+++ format/chris_formatter.py
@@ -0,0 +1,8 @@
+#!/bin/env python
+
+inpt1 = '../dedupe-examples/dedupe-examples-0.5/csv_example/csv_example_messy_input.csv'
+otpt1 = 'input.txt'
+inpt2 = '../dedupe/output.csv'
+otpt2 = 'output.txt'
+
+
